fa0c1c8b61a698f40970104cae62d2d69b2e444d
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 15:06:02 +0100

    Changed to print f.

--- tutorial1/src/tut5/Application.java
+++ tutorial1/src/tut5/Application.java
@@ -4,7 +4,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 5; i++) {
-			System.out.println("The value of i is: " + i);
+			System.out.printf("The value of i is: %d ", i);
 		}
 
 	}
