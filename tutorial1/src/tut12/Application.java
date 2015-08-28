1cba55ffb0b569161a83c67bf85063efc2b595b9
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 20:46:28 +0100

    tut12 rows

---
+++ tutorial1/src/tut12/Application.java
@@ -0,0 +1,24 @@
+package tut12;
+
+public class Application {
+	public static void main(String[] args) {
+
+		int[] values = {3, 5, 2343};
+		
+//		 System.out.println(values[2]);
+		
+		int[][] grid = {
+			{3, 5, 2343},
+			{2,4},
+			{1,2,3,4}
+		};
+		
+		System.out.println(grid[0][2]);
+		
+		for(int row=0; row<grid.length; row++) {
+				for(int col=0; col < grid[row].length; col++) {
+					System.out.print(grid[row][col] + "\t");
+				}
+		}
+	}
+}
