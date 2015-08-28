deb33c37d29ac6d779acfd3c5261796fc4d6e472
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 16:10:46 +0100

    Else if stuff

---
+++ tutorial1/src/tut6/Application.java
@@ -0,0 +1,19 @@
+package tut6;
+
+public class Application {
+	public static void main(String[] args) {
+		
+		int myInt = 99;
+		
+		if(myInt < 10) {
+			System.out.println("Yes, it's true!");
+		}
+		else if(myInt < 50){
+			System.out.println("False!");
+		}
+		else {
+			System.out.println("Both false!");
+		}
+		
+	}
+}
