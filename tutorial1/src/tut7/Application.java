f7a141a26a9109dd572cea2dedc06c76aecd0440
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 17:46:46 +0100

    Changed to number using nextInt

--- tutorial1/src/tut7/Application.java
+++ tutorial1/src/tut7/Application.java
@@ -9,13 +9,13 @@ public class Application {
 		Scanner input = new Scanner(System.in);
 		
 		// Output the prompt
-		System.out.println("Enter a line of text");
+		System.out.println("Enter a number: ");
 		
 		//Wait for the user to enter a line of text
-		String line = input.nextLine();
+		int value = input.nextInt();
 		
 		// Tell them what they entered
-		System.out.println("You entered: " + line);
+		System.out.println("You entered: " + value);
 
 	}
 }
