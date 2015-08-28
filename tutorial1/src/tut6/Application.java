e8338be0c711f7bfb7c45c18bfd51fe62c5e6d39
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 17:23:51 +0100

    New tutorials including a scanner in tut 7

--- tutorial1/src/tut6/Application.java
+++ tutorial1/src/tut6/Application.java
@@ -3,16 +3,16 @@ package tut6;
 public class Application {
 	public static void main(String[] args) {
 		
-		int myInt = 99;
+		int loop = 0;
 		
-		if(myInt < 10) {
-			System.out.println("Yes, it's true!");
-		}
-		else if(myInt < 50){
-			System.out.println("False!");
-		}
-		else {
-			System.out.println("Both false!");
+		while(loop < 5) {
+			System.out.println("Looping: " + loop);
+			
+//			if(loop == 5) {
+//				break;
+//			}
+			
+			loop++;
 		}
 		
 	}
