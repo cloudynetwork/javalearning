ca77c77f82cd6304de1aa11f52807310662b8fbc
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 14:44:46 +0100

    Added string tutorial 3

--- tutorial1/src/tutorial1/Application.java
+++ tutorial1/src/tutorial1/Application.java
@@ -4,7 +4,26 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-
+		int myNumber;
+		short myShort = 488;
+		long myLong = 3322;
+		double myDouble = 33453235;
+		myNumber = 88;
+		
+		float myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false; 
+		byte myByte = 127; 
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
 	}
 
 }
