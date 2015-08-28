8a57fb4183a8bf4dffff89bffa7576ba9a272b38
Peter Joseph <peter@cloudynetwork.org>
2015-08-28 15:05:17 +0100

    Added more tutorials.

---
+++ tutorial1/src/tut5/Application.java
@@ -0,0 +1,11 @@
+package tut5;
+
+public class Application {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("The value of i is: " + i);
+		}
+
+	}
+}
---
+++ tutorial1/src/tutorial3/Application.java
@@ -0,0 +1,27 @@
+package tutorial3;
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println("Hello" + " " + "Bob");
+		
+		System.out.println("My interger is: " + myInt);
+		
+		double myDouble = 7.8;
+		
+		System.out.println("My number is: " + myDouble + "." );
+		
+	
+}
+	
+
+}
