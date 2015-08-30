83f017b3f879645aac7c0095996b26713d69b14f
Peter Joseph <peter@cloudynetwork.org>
2015-08-30 11:03:11 +0100

    Addded some demo instance counter.

---
+++ tutorial1/src/tut24/Application.java
@@ -0,0 +1,54 @@
+package tut24;
+
+class Thing {
+	
+	public final static int LUCKY_NUMBER = 7;
+	
+	public String name;
+	public static String description;
+	public static int count = 0;
+	
+	public Thing() {
+		count++; 
+	}
+	
+	public void showName() {
+		System.out.println(name);
+	}
+	
+	public static void showInfo() {
+		System.out.println("Hello");
+		System.out.println(description);
+	}
+	
+}
+
+public class Application {
+	public static void main(String[] args) {
+
+		Thing.description = "I am a thing";
+		
+		System.out.println("Before creating objects count is: " + Thing.count);
+		
+		System.out.println(Thing.description);
+		
+		Thing thing1 = new Thing();
+		Thing thing2 = new Thing();
+		
+		thing1.name = "Bob";
+		thing2.name = "Sue";
+		
+		thing1.showName();
+		
+		Thing.showInfo();
+		
+		System.out.println(Thing.LUCKY_NUMBER);
+		
+		System.out.println("After creating objects count is: " + Thing.count);
+
+		
+		
+		
+	}
+
+}
