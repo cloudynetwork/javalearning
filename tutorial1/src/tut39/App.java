5ebbac470c1fc96572c098b36f82a474af9f2d8f
Peter Joseph <peter@cloudynetwork.org>
2015-09-08 19:31:13 +0100

    exceptions tut

---
+++ tutorial1/src/tut39/App.java
@@ -0,0 +1,26 @@
+package tut39;
+
+public class App {
+
+	public static void main(String[] args) {
+
+//		int value = 7;
+//		
+//		value = value/0;
+		
+//		String text = null;
+//		
+//		System.out.println(text.length());
+		
+		String[] texts = {"one", "two", "three"};
+		
+		try {
+		System.out.println(texts[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.toString());
+		}
+		
+	}
+	
+	
+}
