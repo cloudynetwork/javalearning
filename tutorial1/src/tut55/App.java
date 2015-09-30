a00a3cd4b6fb8dde14126f18877e38f849b46a19
Peter Joseph <peter@cloudynetwork.org>
2015-09-30 17:22:49 +0100

    Finished Tut 55

--- tutorial1/src/tut55/App.java
+++ tutorial1/src/tut55/App.java
@@ -7,6 +7,31 @@ import java.util.Set;
 import java.util.TreeSet;
 
 class Person implements Comparable<Person> {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 	private String name;
 	
 	public Person(String name) {
@@ -19,8 +44,19 @@ class Person implements Comparable<Person> {
 
 	@Override
 	public int compareTo(Person person) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int len1 = name.length();
+		int len2 = person.name.length();
+		
+		if (len1 > len2) {
+			return 1;
+		}
+		else if (len1 < len2) {
+			return -1;
+		}
+		else {
+		return name.compareTo(person.name);
+		}
 	}
 }
 
